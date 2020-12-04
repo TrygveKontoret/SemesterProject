17d70934213ab8a074ebb0dcfecc1019cc03bf3a
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2020-12-04 20:36:39 +0100

    fikks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".burgerjs");
 const navbar = document.querySelector(".navbar");
 
 hamburger.addEventListener("click", function() {
